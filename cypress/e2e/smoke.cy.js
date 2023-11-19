aa3c3a0461b1c6ff8d604f1181872dba88721c48
Nicole <nxgrizzle@gmail.com>
2023-11-19 14:23:50 -0800

    updated packages, added babel and enzyme

--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -1,5 +1,5 @@
 describe("Smoke Test", () => {
   it("Makes sure that the welcome message comes up", () => {
-    cy.visit("http://localhost:63335").contains("Hello World!");
+    cy.visit("http://localhost:1234").contains("Hello World!");
   });
 });
