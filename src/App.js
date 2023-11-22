845ce1fa401df243dde4e9b6b87298b7632d5d51
Nicole <nxgrizzle@gmail.com>
2023-11-22 12:07:53 -0800

    part 2 of video

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
 import React, { useState } from "react";
-import { incrementCount, decrementCount } from "./utils/counter";
 const App = () => {
-  const [state, setState] = useState(0);
   return (
     <div>
-      <h1 className="hello">Hello World!</h1>
-      <h2 className="counter">{state}</h2>
-      <button
-        className="button"
-        onClick={() => setState((prev) => incrementCount(prev))}
-      >
-        Increment
-      </button>
-      <button
-        className="decrement"
-        onClick={() => setState((prev) => decrementCount(prev))}
-      >
-        Decrement
-      </button>
+      <FilterableRestaurantTable />
     </div>
   );
 };
 export default App;
+
+const FormStates = {
+  VIEW: "view",
+  EDIT: "edit",
+};
+
+export const RestaurantTable = ({ restaurants }) => {
+  if (restaurants.length === 0) return <div>No restaurants</div>;
+  return (
+    <ul>
+      {restaurants.map((restaurant) => {
+        return (
+          <li>
+            {" "}
+            <RestaurantRow key={restaurant.id} restaurant={restaurant.name} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export const RestaurantRow = ({ restaurant }) => {
+  return <div data-testid="restaurantName">{restaurant}</div>;
+};
+
+export const RestaurantForm = ({ addRestaurant }) => {
+  const [state, setState] = useState(FormStates.VIEW);
+  const [restaurantName, setRestaurantName] = useState("");
+
+  const handleAdd = () => {
+    setState(FormStates.EDIT);
+  };
+  const handleChange = (e) => {
+    setRestaurantName(e.target.value);
+  };
+  const handleEdit = () => {
+    addRestaurant(restaurantName);
+    setState(FormStates.VIEW);
+    setRestaurantName("");
+  };
+  return (
+    <div>
+      <button onClick={handleAdd} data-testid="addRestaurantButton">
+        Add Restaurant
+      </button>
+      {state === FormStates.EDIT ? (
+        <div>
+          <input
+            onChange={handleChange}
+            data-testid="newRestaurantName"
+          ></input>
+          <button onClick={handleEdit} data-testid="saveRestaurantButton">
+            Save
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export const FilterableRestaurantTable = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const addRestaurant = (restaurant) => {
+    setRestaurants((prev) => [
+      ...prev,
+      { name: restaurant, id: Math.random() * 10000000 },
+    ]);
+  };
+  return (
+    <div>
+      <RestaurantForm addRestaurant={addRestaurant} />
+      <RestaurantTable restaurants={restaurants} />
+    </div>
+  );
+};
--- test/unit/App.spec.js
+++ test/unit/App.spec.js
@@ -1,22 +1,72 @@
 import React from "react";
 import { mount } from "enzyme";
-import App from "../../src/App";
+import App, {
+  RestaurantForm,
+  RestaurantRow,
+  RestaurantTable,
+} from "../../src/App";
 
-describe("App", () => {
-  it("renders hello world", () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find(".hello").text()).toBe("Hello World!");
-    expect(wrapper.find(".counter").text()).toBe("0");
-    expect(wrapper.find(".button").text()).toBe("Increment");
+describe("RestaurantForm", () => {
+  const addRestaurant = jest.fn();
+  const wrapper = mount(<RestaurantForm addRestaurant={addRestaurant} />);
+  it("has a button to add a restaurant", () => {
+    expect(wrapper.find("[data-testid='addRestaurantButton']")).toHaveLength(1);
   });
-  it("increments the counter when the button is clicked", () => {
-    const wrapper = mount(<App />);
-    wrapper.find(".button").simulate("click");
-    expect(wrapper.find(".counter").text()).toBe("1");
+  it("does not have a button to save a restaurant", () => {
+    expect(wrapper.find("[data-testid='saveRestaurantButton']")).toHaveLength(
+      0
+    );
   });
-  it("decrements the counter when the button is clicked", () => {
-    const wrapper = mount(<App />);
-    wrapper.find(".decrement").simulate("click");
-    expect(wrapper.find(".counter").text()).toBe("-1");
+
+  it("has a button to add a restaurant", () => {
+    expect(wrapper.find("[data-testid='addRestaurantButton']")).toHaveLength(1);
+    wrapper.find("[data-testid='addRestaurantButton']").simulate("click");
+    expect(wrapper.find("[data-testid='saveRestaurantButton']")).toHaveLength(
+      1
+    );
+  });
+  it("calls addRestaurant when save is clicked and hides input", () => {
+    wrapper.find("[data-testid='addRestaurantButton']").simulate("click");
+    wrapper.find("[data-testid='newRestaurantName']").simulate("change", {
+      target: { value: "Sushi Place" },
+    });
+    wrapper.find("[data-testid='saveRestaurantButton']").simulate("click");
+    expect(addRestaurant).toHaveBeenCalledWith("Sushi Place");
+    expect(wrapper.find('[data-testid="newRestaurantName"]')).toHaveLength(0);
+  });
+});
+
+describe("RestaurantRow", () => {
+  it("renders text given", () => {
+    const wrapper = mount(<RestaurantRow restaurant="Sushi Place" />);
+    expect(wrapper.find("[data-testid='restaurantName']").text()).toBe(
+      "Sushi Place"
+    );
+  });
+});
+
+describe("RestaurantTable", () => {
+  it('renders "No restaurants"', () => {
+    const wrapper = mount(<RestaurantTable restaurants={[]} />);
+    expect(wrapper.text()).toBe("No restaurants");
+  });
+
+  it("renders a single restaurant", () => {
+    const wrapper = mount(
+      <RestaurantTable restaurants={[{ id: 1, name: "Sushi Place" }]} />
+    );
+    expect(wrapper.find("[data-testid='restaurantName']")).toHaveLength(1);
+  });
+
+  it("renders multiple restaurants", () => {
+    const wrapper = mount(
+      <RestaurantTable
+        restaurants={[
+          { id: 1, name: "Sushi Place" },
+          { id: 2, name: "Pizza Place" },
+        ]}
+      />
+    );
+    expect(wrapper.find("[data-testid='restaurantName']")).toHaveLength(2);
   });
 });
